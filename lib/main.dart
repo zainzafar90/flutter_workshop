e39702c079cd69489aa2a1d61f489b670b4fbc92
Zain Zafar <zain@aurorasolutions.io>
2019-07-04 00:56:39 +0500

    create startup name part one list

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:english_words/english_words.dart';
+
 void main() => runApp(App());
 
 class App extends StatefulWidget {
@@ -8,22 +10,51 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  final items = List<String>.generate(200, (i) => "Item ${i + 1}");
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: Text('List View')),
-        body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${items[index]}'),
-            );
-          },
-        ),
+        body: RandomWords(),
       ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final wordPair = WordPair.random();
+  final List<WordPair> _suggestions = <WordPair>[];
+
+  buildListItem(WordPair wordPair) {
+    return ListTile(
+      title: Text(
+        wordPair.asPascalCase,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        final int index = i ~/ 2;
+        print(index);
+        if (index >= _suggestions.length) {
+          // ...then generate 10 more and add them to the
+          // suggestions list.
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return buildListItem(_suggestions[index]);
+      },
     );
   }
 }
