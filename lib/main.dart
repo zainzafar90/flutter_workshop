15e2b52e48b4f233fb5e6e91ef383b595e62e5c7
Zain Zafar <zain@aurorasolutions.io>
2019-07-03 21:26:12 +0500

    Add Step 03

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,30 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  var counter = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Hello World Application",
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        appBar: AppBar(title: Text("Hello World Flutter")),
         body: Center(
-          child: Text("Hello world"),
+          child: Text(counter.toString()),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              counter++;
+            });
+            print(counter);
+          },
+          child: Icon(Icons.add),
         ),
       ),
     );
